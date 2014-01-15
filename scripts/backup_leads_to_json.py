82e9f73d46a423f26be2b67fc6c9dab448e8c337
Anthony Nemitz <anemitz@gmail.com>
2014-01-15 10:21:35 -0800

    include activities in lead json dump

--- scripts/backup_leads_to_json.py
+++ scripts/backup_leads_to_json.py
@@ -4,7 +4,7 @@ import codecs
 from closeio_api import Client as CloseIO_API
 
 """
-Dumps all leads within a given organization to a file in JSON format
+Dumps all leads within a given organization to a file in JSON format, including activities
 """
 
 api_key = raw_input("API key: ")
@@ -12,18 +12,30 @@ file_name = raw_input("Output Filename: ")
 
 api = CloseIO_API(api_key)
 
-f = codecs.open(file_name, "w", "utf-8")
-
-leads = []
+output = []
 has_more = True
 offset = 0
-while has_more:
-    resp = api.get('lead', data={'_skip': offset})
-    data = resp['data']
-    for lead in data:
-        leads.append(lead)
-    offset += len(data)
-    has_more = resp['has_more']
 
-json.dump(leads, f)
-f.close()
+with codecs.open(file_name, "w", "utf-8") as f:
+    while has_more:
+        resp = api.get('lead', data={'_skip': offset})
+        leads = resp['data']
+
+        offset += len(leads)
+        has_more = resp['has_more']
+
+        for lead in leads:
+            more_activities = True
+            activities_offset = 0
+            lead['activities'] = []
+
+            while more_activities:
+                resp = api.get('activity', data={'lead_id': lead['id'], '_skip': activities_offset})
+                activities = resp['data']
+                lead['activities'].extend(activities)
+                activities_offset += len(activities)
+                more_activities = resp['has_more']
+
+            output.append(lead)
+
+    json.dump(output, f)
