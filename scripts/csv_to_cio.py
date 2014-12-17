09e1e47707aef04e8028c99452e77dd4897d56b6
Phil Freo <phil@philfreo.com>
2014-12-17 12:45:25 -0800

    more fixes to script

--- scripts/csv_to_cio.py
+++ scripts/csv_to_cio.py
@@ -4,6 +4,7 @@ import time, argparse, re, unidecode, sys, json
 from progressbar import ProgressBar
 from progressbar.widgets import Percentage, Bar, ETA, FileTransferSpeed
 from requests.exceptions import ConnectionError
+import closeio_api
 from closeio_api import Client as CloseIO_API
 from closeio_api.utils import CsvReader, count_lines, title_case, uncamel
 
