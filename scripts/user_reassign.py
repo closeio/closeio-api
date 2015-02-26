ea7adc03c788986de2562132c32a29cc3280c287
Alexander Deynichenko <adeynichenko@gmail.com>
2015-02-27 00:21:35 +0700

    added summary line for errors; added --continue-on-error option

--- scripts/user_reassign.py
+++ scripts/user_reassign.py
@@ -4,7 +4,7 @@
 import argparse
 import logging
 
-from closeio_api import Client as CloseIO_API
+from closeio_api import Client as CloseIO_API, APIError
 
 parser = argparse.ArgumentParser(description='Assigns tasks or opportunities from one user to another')
 group_from = parser.add_mutually_exclusive_group(required=True)
@@ -19,6 +19,7 @@ parser.add_argument('--development', '-d', action='store_true',
                     help='Use a development (testing) server rather than production.')
 parser.add_argument('--confirmed', '-c', action='store_true',
                     help='Without this flag, the script will do a dry run without actually updating any data.')
+parser.add_argument('--continue-on-error', '-s', action='store_true', help='Do not abort after first error')
 group = parser.add_argument_group()
 group.add_argument('--tasks', '-T', action='store_true', help='reassign only non complete tasks')
 group.add_argument('--all-tasks', action='store_true', help='reassign all tasks')
@@ -79,62 +80,79 @@ logging.info('to user_id: %s (%s)' % (to_user_id, ids_to_emails[to_user_id]))
 
 assert from_user_id != to_user_id, 'equal user codes'
 
+opportunities_errors = 0
+tasks_errors = 0
+try:
+    # tasks
+    updated_tasks = 0
+    if args.tasks or args.all_tasks:
+        has_more = True
+        offset = 0
+        while has_more:
+            payload = {
+                'assigned_to': from_user_id,
+                '_order_by': 'date_created',
+                '_skip': offset,
+                '_fields': 'id'
+            }
 
-# tasks
-updated_tasks = 0
-if args.tasks or args.all_tasks:
-    has_more = True
-    offset = 0
-    while has_more:
-        payload = {
-            'assigned_to': from_user_id,
-            '_order_by': 'date_created',
-            '_skip': offset,
-            '_fields': 'id'
-        }
+            if not args.all_tasks:
+                payload['is_complete'] = False
 
-        if not args.all_tasks:
-            payload['is_complete'] = False
-            
-        resp = api.get('task', data=payload)
-        
-        tasks = resp['data']
-        for task in tasks:
-            if args.confirmed:
-                api.put('task/'+task['id'], data={'assigned_to': to_user_id})
-            logging.info('updated task %s' % task['id'])
-            updated_tasks += 1
+            resp = api.get('task', data=payload)
 
-        offset += max(0, len(tasks) - 1)
-        has_more = resp['has_more']
+            tasks = resp['data']
+            for task in tasks:
+                if args.confirmed:
+                    try:
+                        api.put('task/'+task['id'], data={'assigned_to': to_user_id})
+                    except APIError as e:
+                        tasks_errors += 1
+                        if not args.continue_on_error:
+                            raise e
+                        logging.error('task %s skipped with error %s' % (task['id'], e))
+                logging.info('updated %s' % task['id'])
+                updated_tasks += 1
 
-# opportunities
-updated_opportunities = 0
-if args.opportunities or args.all_opportunities:
-    has_more = True
-    offset = 0
-    while has_more:
-        payload = {
-            'user_id': from_user_id,
-            '_order_by': 'date_created',
-            '_skip': offset,
-            '_fields': 'id'
-        }
+            offset += max(0, len(tasks) - 1)
+            has_more = resp['has_more']
 
-        if not args.all_opportunities:
-            payload['status_type'] = 'active'
+    # opportunities
+    updated_opportunities = 0
+    if args.opportunities or args.all_opportunities:
+        has_more = True
+        offset = 0
+        while has_more:
+            payload = {
+                'user_id': from_user_id,
+                '_order_by': 'date_created',
+                '_skip': offset,
+                '_fields': 'id'
+            }
 
-        resp = api.get('opportunity', data=payload)
+            if not args.all_opportunities:
+                payload['status_type'] = 'active'
 
-        opportunities = resp['data']
-        for opportunity in opportunities:
-            if args.confirmed:
-                api.put('opportunity/'+opportunity['id'], data={'user_id': to_user_id})
-            logging.info('updated opportunity %s' % opportunity['id'])
-            updated_opportunities += 1
+            resp = api.get('opportunity', data=payload)
 
-        offset += max(0, len(opportunities) - 1)
-        has_more = resp['has_more']
+            opportunities = resp['data']
+            for opportunity in opportunities:
+                if args.confirmed:
+                    try:
+                        api.put('opportunity/'+opportunity['id'], data={'user_id': to_user_id})
+                    except APIError as e:
+                        opportunities_errors += 1
+                        if not args.continue_on_error:
+                            raise e
+                        logging.error('opportunity %s skipped with error %s' % (opportunity['id'], e))
+                logging.info('updated %s' % opportunity['id'])
+                updated_opportunities += 1
+
+            offset += max(0, len(opportunities) - 1)
+            has_more = resp['has_more']
+except APIError:
+    logging.error('stopped on error %s' % e)
 
 logging.info('summary: updated tasks %d, updated opportunities %d' % (updated_tasks, updated_opportunities))
-
+if opportunities_errors or tasks_errors:
+    logging.info('summary: tasks errors: %s, opportunities errors %d' % (tasks_errors, opportunities_errors))
