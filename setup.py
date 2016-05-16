82c0324a9213e81acaf6458c07a00d8dbfa7287f
Stefan Wojcik <wojcikstefan@gmail.com>
2016-05-16 10:29:58 -0700

    nicer setup.py and blist ver bump

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup
 requires = ['requests >= 0.10.8', 'grequests >= 0.2.0', 'six==1.9.0']
 
 setup(
-    name = "closeio",
-    packages = ['closeio_api'],
-    version = "0.1",
-    description = "Close.io Python Library",
-    author = "Close.io Team",
-    url = "https://github.com/closeio/closeio-api/",
-    install_requires = requires,
-    classifiers = [
+    name="closeio",
+    packages=['closeio_api'],
+    version="0.2",
+    description="Close.io Python Library",
+    long_description="Closeio Python library",
+    author="Close.io Team",
+    url="https://github.com/closeio/closeio-api/",
+    install_requires=requires,
+    classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
@@ -18,7 +19,5 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Operating System :: OS Independent",
-        ],
-    long_description = """\
-        Closeio Python library
-         """ )
+    ]
+)
