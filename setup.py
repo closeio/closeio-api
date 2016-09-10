6f57bc5ff8373b47c0d2338d400c427ea323697d
Anthony Nemitz <anemitz@gmail.com>
2016-09-10 11:25:21 -0600

    remove six from setup, bump version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-requires = ['requests >= 0.10.8', 'grequests >= 0.2.0', 'six==1.9.0']
+requires = ['requests >= 2.11.1', 'grequests >= 0.3.0']
 
 setup(
     name="closeio",
     packages=['closeio_api'],
-    version="0.3",
+    version="0.4",
     description="Close.io Python Library",
     long_description="Closeio Python library",
     author="Close.io Team",
