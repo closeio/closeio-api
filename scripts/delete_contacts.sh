a3af0db70f1177025ecfc3c873827b4ad4d771f3
Anthony Nemitz <anemitz@gmail.com>
2016-09-15 11:48:37 -0600

    adding a barebones bash script to delete contacts

---
+++ scripts/delete_contacts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+OLDIFS=$IFS
+API_KEY=<YOUR_API_KEY>
+IFS=,
+i=1
+[ ! -f $1 ] && { echo "$1 file not found"; exit -1; }
+while read contact_id rest_of_shit
+do
+  test $i -eq 1 && ((i=i+1)) && continue
+  echo "Contact ID: $contact_id"
+  curl -XDELETE "https://app.close.io/api/v1/contact/$contact_id/" -u $API_KEY:
+done < $1
+IFS=$OLDIFS
