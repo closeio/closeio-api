b1b751243e3c06ea962dff34e07cf2049cc7d110
Eric Engoron <ericengoron@Erics-iMac.local>
2017-03-23 00:54:38 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+requires = ['requests >= 2.11.1', 'grequests >= 0.3.0']
+
+setup(
+    name="closeio",
+    packages=['closeio_api'],
+    version="0.5",
+    description="Close.io Python Library",
+    long_description="Closeio Python library",
+    author="Close.io Team",
+    url="https://github.com/closeio/closeio-api/",
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+    ]
+)
