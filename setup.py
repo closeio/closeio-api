139f1a7f6f144e3f0f84b6931668253d052ee649
Eric Engoron <eric.engoron@gmail.com>
2019-12-13 17:42:24 -0500

    Add custom User Agent to every request, and expose the version in a readable way (#104)
    * Add User Agent to requests, add the ability for setup.py to take the version number from __init__.py, and update the version number to 1.2
    
    * Update User Agent format to RFC 7231 standard

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import io
+import re
 from setuptools import setup
+
+VERSION_FILE = "closeio_api/__init__.py"
+with io.open(VERSION_FILE, "rt", encoding="utf8") as f:
+    version = re.search(r'__version__ = ([\'"])(.*?)\1', f.read()).group(2)
 
 setup(
     name="closeio",
     packages=['closeio_api'],
-    version="1.1",
+    version=version,
     description="Close API Python Client",
     long_description="Close API Python Client",
     author="Close Team",
